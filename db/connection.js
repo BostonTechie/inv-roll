57856b738b70ceb198a2d55fa4d5dd17759ff5fa
andrewurq <andrewurq@gmail.com>
2022-01-13 14:16:11 -0500

    changing env db

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose')
 
 
-const mongoURI =
-  //check if the node environment is production
-  process.env.NODE_ENV === "production"
-    ? //if so, use DB_URL as the database location
-      process.env.DB_URL
-    : //if not, use the fruits-app db on the MongoDB's local server
-      "mongodb://localhost/invest-roll";
+const mongoURI = process.env.mongoURI
 
 
 mongoose.connect(mongoURI, {
